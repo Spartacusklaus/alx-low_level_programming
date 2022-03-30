c9e596dc2ac1954e626ecd23860341c3b28e1779
Your Name <spartacusklaus45@gmail.com>
2022-03-30 05:42:51 -0700

    My commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
-*main - entry point
-*
-*Return : O -after printing the function
-*/
+  * main - Prints a text
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
-put ("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
